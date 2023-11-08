86b94d8ec00ef45910a0e3c3ad1ca9df81feba03
Cristhian Niño <saratos666@gmail.com>
2023-11-08 08:28:49 -0500

    primer commit

---
+++ src/views/MejorCalificadosView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4" v-for="pelicula in page.results" :key="pelicula.id">
+          <div class="card">
+            <img :src="'https://image.tmdb.org/t/p/w500' + pelicula.backdrop_path" class="card-img-top" :alt="pelicula.title" />
+            <div class="card-body">
+              <h5 class="card-title">{{ pelicula.title }}</h5>
+              <p class="card-text">{{ pelicula.overview }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+import PeliService from '@/service/PeliService'
+  
+  export default {
+    name: 'MejorCalificadosView',
+    data() {
+      return {
+        page: {
+          results: [],
+        },
+      };
+    },
+    async created() {
+      const peliculaId = this.$route.params.id;
+      this.page = await PeliService.getMejorCalificados();
+      console.log(this.page);
+    },
+  };
+  </script>
