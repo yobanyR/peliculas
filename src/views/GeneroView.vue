3f524efa42be26664318950ad7c7aa2f9160788e
junior <2unior@123.com>
2023-11-10 04:18:47 -0500

    conflictos resueltos

--- src/views/GeneroView.vue
+++ src/views/GeneroView.vue
@@ -1,23 +1,4 @@
 <template>
-    <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">GENERO</h5>
-    <p class="card-text">{{ movieDetails.genres }}</p>
-  </div>
-</div>
-</template> 
-<script>
-import PeliService from '@/service/PeliService';
-import { useRoute } from 'vue-router';
-export default{
-    name:'ResumenView',
-    data (){
-        return {
-            id:'',
-            movieDetails:{},
-            genres:{}
-
-        }
     <div>
       <h4>Género</h4>
       <p v-if="movieDetails.genres">{{ movieDetails.genres.map(genre => genre.name).join(', ') }}</p>
--- src/views/ResumenView.vue
+++ src/views/ResumenView.vue
@@ -1,21 +1,4 @@
 <template>
-    <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Resumen</h5>
-    <p class="card-text">{{ movieDetails.overview }}</p>
-  </div>
-</div>
-</template> 
-<script>
-import PeliService from '@/service/PeliService';
-import { useRoute } from 'vue-router';
-export default{
-    name:'ResumenView',
-    data (){
-        return {
-            id:'',
-            movieDetails:{}
-        }
     <div>
       <h4>Resumen</h4>
       <p>{{ movieDetails.overview }}</p>
